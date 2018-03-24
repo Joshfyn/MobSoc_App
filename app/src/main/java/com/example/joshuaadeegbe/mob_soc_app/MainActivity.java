90eecb7bc9294def1e83dcc380e260bc91fe8189
Joshfyn <joshuaadeegbe@yahoo.com>
2018-03-24 02:45:30 +0200

    Transfer of menu to Exercice option

--- app/src/main/java/com/example/joshuaadeegbe/mob_soc_app/MainActivity.java
+++ app/src/main/java/com/example/joshuaadeegbe/mob_soc_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.joshuaadeegbe.mob_soc_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,11 +11,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,21 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Button listener to start the fitness/application activities
+        button1 = (Button) findViewById(R.id.startapp_fit);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenExercise_Option();
+
+            }
+        });
+    }
+
+    public void OpenExercise_Option() {
+        Intent intent = new Intent(this, Exercise_Option.class);
+        startActivity(intent);
     }
 
     @Override
@@ -52,27 +69,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
